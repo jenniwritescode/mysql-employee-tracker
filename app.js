de59a68a4235c17cc8261627abb510fd23fdce7f
Jenni <96969934+jenniwritescode@users.noreply.github.com>
2022-04-23 11:30:47 -0500

    add inquirer prompts

--- app.js
+++ app.js
@@ -11,13 +11,62 @@ const connection = mysql.createConnection({
   user: "root",
   password: "ITrOCRUArtuR",
   database: "employee_DB",
-  socketPath: '/tmp/mysql.sock'
+  socketPath: "/tmp/mysql.sock",
 });
 
-connection.connect(function(err) {
-  if (err) throw err
-  console.log("Connected as ID " + connection.threadId)
-  // startPrompt();
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected as ID " + connection.threadId);
+  startPrompt();
 });
 
+// inquirer prompts
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: 'checkbox',
+        message: 'Welcome to the Employee Tracker Database! Make a Selection:',
+        name: 'checkboxChoices',
+        choices: [
+          'View all Employees',
+          'View all Employees By Role',
+          'View all Employees By Department',
+          'Update Employee',
+          'Add an Employee',
+          'Add a Role',
+          'Add a Department',
+        ],
+      },
+    ])
+    .then(function (val) {
+      switch (val.choice) {
+        case 'View all Employees':
+          // viewEmployees();
+          break;
 
+        case 'View all Employees By Role':
+          // viewEmpByRole();
+          break;
+        case 'View all Employees By Department':
+          // viewEmpByDept();
+          break;
+
+        case 'Update Employee':
+          // addEmployee();
+          break;
+
+        case 'Add an Employee':
+          // updateEmployee();
+          break;
+
+        case 'Add a Role':
+          // addRole();
+          break;
+
+        case 'Add a Department':
+          // addDept();
+          break;
+      }
+    });
+}
