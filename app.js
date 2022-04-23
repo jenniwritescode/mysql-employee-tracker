8c869c1459cd7f9089f9a4a462d8805e97c333b4
Jenni <96969934+jenniwritescode@users.noreply.github.com>
2022-04-23 12:11:31 -0500

    add viewEmpByRole function

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function startPrompt() {
           viewEmployees();
           break;
         case 'View all Employees By Role':
-          // viewEmpByRole();
+          viewEmpByRole();
           break;
         case 'View all Employees By Department':
           // viewEmpByDept();
@@ -71,7 +71,7 @@ function startPrompt() {
           break;
       }
     });
-}
+};
 
 // checkboxChoice functions below
 
@@ -80,8 +80,19 @@ function viewEmployees() {
   connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
   function(err, res) {
     if (err) throw err
-    log.green('------ Employees Table ------', '\n');
+    log.green('------ List of All Employees ------', '\n');
     console.table(res);
     startPrompt();
 })
-}
+};
+
+// view all employees by role function
+function viewEmpByRole() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+  function(err, res) {
+  if (err) throw err
+  log.pink('------ List of Employees by Role ------', '\n');
+  console.table(res)
+  startPrompt()
+  })
+};
